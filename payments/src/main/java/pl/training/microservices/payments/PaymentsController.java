a6162f9bcb78080eefb3a112c7a6dbeb34cf4b86
landrzejewski <landrzejewski.poczta@gmail.com>
2020-04-06 15:16:40 +0200

    Gateway

--- payments/src/main/java/pl/training/microservices/payments/PaymentsController.java
+++ payments/src/main/java/pl/training/microservices/payments/PaymentsController.java
@@ -3,10 +3,8 @@ package pl.training.microservices.payments;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1")
 @RestController
 @RequiredArgsConstructor
 public class PaymentsController {
