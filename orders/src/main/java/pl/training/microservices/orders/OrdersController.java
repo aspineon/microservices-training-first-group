4fb462a7bc2731766bf8813be12e121911c508b6
landrzejewski <landrzejewski.poczta@gmail.com>
2020-04-07 15:46:22 +0200

    Api

--- orders/src/main/java/pl/training/microservices/orders/OrdersController.java
+++ orders/src/main/java/pl/training/microservices/orders/OrdersController.java
@@ -3,7 +3,6 @@ package pl.training.microservices.orders;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,11 +23,10 @@ public class OrdersController {
         if (bindingResult.hasErrors()) {
             return ResponseEntity.badRequest().build();
         }
-        throw new ProductNotFoundException();
-//        Order order = ordersMapper.toOrder(orderTo);
-//        Long orderId = ordersService.placeOrder(order);
-//        URI locationUri = uriBuilder.requestUriWithAppendedId(orderId);
-//        return ResponseEntity.created(locationUri).build();
+        Order order = ordersMapper.toOrder(orderTo);
+        Long orderId = ordersService.placeOrder(order);
+        URI locationUri = uriBuilder.requestUriWithAppendedId(orderId);
+        return ResponseEntity.created(locationUri).build();
     }
 
     /*@ExceptionHandler(ProductNotFoundException.class)
