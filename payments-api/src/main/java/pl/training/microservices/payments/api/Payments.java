fed3bd08ff25fd957bcd7cf98d6a74f5c8578037
landrzejewski <landrzejewski.poczta@gmail.com>
2020-04-28 12:09:06 +0200

    gateway

--- payments-api/src/main/java/pl/training/microservices/payments/api/Payments.java
+++ payments-api/src/main/java/pl/training/microservices/payments/api/Payments.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+//@FeignClient("http://payments-service")
 @FeignClient("payments-service")
 public interface Payments {
 
