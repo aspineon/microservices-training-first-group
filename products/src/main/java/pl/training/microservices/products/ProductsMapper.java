df647b6cfef691d3f6c7d1762261552b159df105
landrzejewski <landrzejewski.poczta@gmail.com>
2020-04-28 10:01:12 +0200

    gateway

--- products/src/main/java/pl/training/microservices/products/ProductsMapper.java
+++ products/src/main/java/pl/training/microservices/products/ProductsMapper.java
@@ -1,19 +1,15 @@
 package pl.training.microservices.products;
 
-import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import pl.training.microservices.products.api.ProductTo;
 import pl.training.microservices.products.api.ResultPageTo;
-
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProductsMapper {
 
     Product toProduct(ProductTo productTo);
 
-    @IterableMapping(elementTargetType = ProductTo.class)
-    List<ProductTo> toProductTo(List<Product> products);
+    ProductTo toProductTo(Product product);
 
     ResultPageTo<ProductTo> toResultPageTo(ResultPage<Product> resultPage);
 
