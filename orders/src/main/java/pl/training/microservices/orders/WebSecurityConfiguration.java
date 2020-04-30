572fd73cc48474d244602cf72980a702dee37dfb
landrzejewski <landrzejewski.poczta@gmail.com>
2020-04-30 15:21:24 +0200

    gateway

---
+++ orders/src/main/java/pl/training/microservices/orders/WebSecurityConfiguration.java
@@ -0,0 +1,42 @@
+package pl.training.microservices.orders;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfiguration {
+
+    @Value("${jwtSigningKey}")
+    @Setter
+    private String jwtSigningKey;
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        CustomJwtAccessTokenConverter tokenConverter = new CustomJwtAccessTokenConverter();
+        tokenConverter.setSigningKey(jwtSigningKey);
+        return tokenConverter;
+    }
+
+    @Bean
+    public TokenStore tokenStore(JwtAccessTokenConverter jwtAccessTokenConverter) {
+        return new JwtTokenStore(jwtAccessTokenConverter);
+    }
+
+    @Primary
+    @Bean
+    public DefaultTokenServices tokenServices(TokenStore tokenStore) {
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setTokenStore(tokenStore);
+        return tokenServices;
+    }
+
+}
