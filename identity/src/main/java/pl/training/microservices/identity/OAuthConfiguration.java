4384aac9540c811dd5f1f932c3f2bf396cd314ad
landrzejewski <landrzejewski.poczta@gmail.com>
2020-04-30 12:30:06 +0200

    gateway

---
+++ identity/src/main/java/pl/training/microservices/identity/OAuthConfiguration.java
@@ -0,0 +1,48 @@
+package pl.training.microservices.identity;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+@EnableAuthorizationServer
+@Configuration
+@RequiredArgsConstructor
+public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
+
+
+    private final JwtAccessTokenConverter jwtAccessTokenConverter;
+    private final TokenStore tokenStore;
+    private final UsersService usersService;
+    private final AuthenticationManager authenticationManagerBean;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.accessTokenConverter(jwtAccessTokenConverter)
+                .tokenStore(tokenStore)
+                .authenticationManager(authenticationManagerBean)
+                .userDetailsService(usersService);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.allowFormAuthenticationForClients().passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("training")
+                .authorizedGrantTypes("password")
+                .scopes("api");
+    }
+
+}
